fa15964d421e0d631fde312cc7171dda419f5f88
nshadybekov <shadybekov@cointelegraph.com>
2022-07-10 18:46:53 +0300

    removed useless async

--- index.js
+++ index.js
@@ -8,12 +8,11 @@ const bot = new Telegraf(process.env.BOT_TOKEN)
 bot.start((ctx) => ctx.reply('Welcome'))
 bot.help((ctx) => ctx.reply('Send me a sticker'))
 bot.on('sticker', (ctx) => ctx.reply('👍'))
-bot.hears(/([.])\w{0,4}/, async (ctx) => {
+bot.hears(/([.])\w{0,4}/,(ctx) => {
   ctx.reply('Hey there')
   fetch('https://jsonplaceholder.typicode.com/photos/1')
       .then(res => res.json())
       .then(data => {
-        console.log(data)
         ctx.telegram.sendMessage(channels.shadybekov_channel_2, JSON.stringify(data))
       })
       .catch(err => console.log(err))
