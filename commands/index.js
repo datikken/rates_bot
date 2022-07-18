36098af50514ac96a045cd46990cdf9b4e98e9ca
nshadybekov <shadybekov@cointelegraph.com>
2022-07-18 22:52:53 +0300

    get btc price

--- commands/index.js
+++ commands/index.js
@@ -1,5 +1,6 @@
 import {Markup} from "telegraf";
 import {getAllCountryButtons, getAllTasksButtons} from "../database/button.js";
+import fetch from 'node-fetch';
 
 export const setBotCommands = (bot) => {
   bot.command('/create_task', async ctx => {
@@ -20,5 +21,13 @@ export const setBotCommands = (bot) => {
     } catch (e) {
       console.error(e)
     }
+  })
+
+  bot.command('/btc', async ctx => {
+      const resp = await fetch(`${process.env.TICKER_URL}/BTC/`)
+      const { data } = await resp.json();
+      const answer = `Bitcoin BTC - ${data.USD.price}$ ${data.USD.day}%`;
+
+      ctx.reply(answer)
   })
 }
