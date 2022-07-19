f936b5cc20e63d782426d83340b093bb64944011
nshadybekov <shadybekov@cointelegraph.com>
2022-07-19 22:19:42 +0300

    hello message

--- commands/index.js
+++ commands/index.js
@@ -2,10 +2,16 @@ import {Markup} from "telegraf";
 import {getAllCountryButtons, getAllTasksButtons} from "../database/button.js";
 import {getCoinPrice} from "../ticker/index.js";
 import {getFormatedMessage} from "../util/index.js";
+import {cmmnds} from "./commands.js";
 
-//TODO add start message
 
 export const setBotCommands = (bot) => {
+  bot.start((ctx) => ctx.replyWithHTML(`
+    Welcome! \n
+    <em>Here what you can do:</em>
+    ${cmmnds.map(el => `<pre>${el}</pre>\n`).join('')}
+  `));
+
   bot.command('/create_task', async ctx => {
     try {
       await ctx.replyWithHTML(`<b>Select country:</b>`, Markup.inlineKeyboard([
