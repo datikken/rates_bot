ebdb57198a0b04815835b35f5be18efddfda7f21
nshadybekov <shadybekov@cointelegraph.com>
2022-07-19 21:03:55 +0300

    bot webhook url

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import "dotenv/config";
 // import { channels } from "./config/channels.js";
 
 const PORT = process.env.PORT || 3000;
-const URL = process.env.URL || 'https://your-heroku-app.herokuapp.com';
+const URL = process.env.URL || 'https://obscure-sea-50068.herokuapp.com';
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 bot.telegram.setWebhook(`${URL}/bot${process.env.BOT_TOKEN}`);
