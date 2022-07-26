bbdd78ec0c55ec41ae4b1da586293aa6a37e80da
nshadybekov <shadybekov@cointelegraph.com>
2022-07-26 23:46:41 +0300

    test webhooks

--- index.js
+++ index.js
@@ -2,12 +2,18 @@ import {Telegraf} from 'telegraf';
 import {runScheduledTasks} from "./schedule/index.js";
 import {setBotActions} from "./actions/index.js";
 import {setBotCommands} from "./commands/index.js";
+import Sentry from '@sentry/node'
 import "dotenv/config";
 
 const PORT = process.env.PORT || 3000;
 const URL = process.env.URL || 'https://obscure-sea-50068.herokuapp.com';
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
+
+Sentry.init({
+  dsn: "https://37818e6c84644246956dd9163b1bdeec@o1152440.ingest.sentry.io/6603786",
+  tracesSampleRate: 1.0,
+});
 
 bot.telegram.setWebhook(`${URL}/bot${process.env.BOT_TOKEN}`);
 bot.startWebhook(`/bot${process.env.BOT_TOKEN}`, null, PORT);
